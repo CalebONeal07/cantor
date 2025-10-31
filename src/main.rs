d1166446d7b350288018a9da745431892da72026
Caleb O'Neal <caleboneal07@gmail.com>
2025-10-30 20:57:27 -0400

    clean slate

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,1 @@
-use cantor::matrix::Matrix;
-
-fn main() {
-    let x = Matrix::<i32, 4, 4>::new([
-        2, 3, 4, 5,
-        6, 7, 8, 9,
-        10, 12, 13, 14,
-        15, 16, 12, 13
-    ]);
-
-    println!("{}", x);
-}
+fn main() {}
